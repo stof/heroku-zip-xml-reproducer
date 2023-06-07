c4d677b9a3f459e5c10b7b81810b4199189b7cd1
Christophe Coevoet <stof@notk.org>
2023-06-07 17:38:23 +0200

    Create a reproducer for the heroku xml in zip issue

---
+++ read_xml.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$filePath = __DIR__ . '/sharedStrings.xml';
+
+$reader = new XMLReader();
+$reader->open($filePath, null, LIBXML_NONET);
+
+libxml_clear_errors();
+$initialUseInternalErrorsValue = libxml_use_internal_errors(true);
+
+$status = $reader->next('sst');
+var_dump($status);
+var_dump(libxml_get_errors());
---
+++ read_xml_zip.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$filePath = 'zip://' . __DIR__ . '/debug.xlsx#xl/sharedStrings.xml';
+
+$reader = new XMLReader();
+$reader->open($filePath, null, LIBXML_NONET);
+
+libxml_clear_errors();
+$initialUseInternalErrorsValue = libxml_use_internal_errors(true);
+
+$status = $reader->next('sst');
+var_dump($status);
+var_dump(libxml_get_errors());
